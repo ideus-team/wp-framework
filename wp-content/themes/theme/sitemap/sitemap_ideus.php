2ba691d80be4c0ca3adcb4f907fd4a987da7535f
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-04-26 18:09:21 +0300

    Внедрить sitemap в wp-framework

---
+++ wp-content/themes/theme/sitemap/sitemap_ideus.php
@@ -0,0 +1,10 @@
+<?
+/*
+Template Name: Sitemap iDeus
+*/
+
+$type = 'ideus';
+$company = 'iDeus';
+$link = 'http://ideus.biz/';
+require('sitemap.php');
+?>
---
+++ wp-content/themes/theme/sitemap/sitemap_netcraft.php
@@ -0,0 +1,10 @@
+<?
+/*
+Template Name: Sitemap Net-Craft
+*/
+
+$type = 'netcraft';
+$company = 'Net-Craft';
+$link = 'http://net-craft.com/';
+require('sitemap.php');
+?>
