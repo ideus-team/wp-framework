032aac6a56534a0a8c4bcf522c6e265d0d57b410
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-05-19 12:47:07 +0300

    Fix searchform

--- wp-content/themes/theme/searchform.php
+++ wp-content/themes/theme/searchform.php
@@ -1,7 +1,7 @@
 <div class="l-siteSearch">
   <form class="b-siteSearch" role="search" action="<?php echo home_url('/'); ?>" method="get">
-    <label class="b-siteSearch__label g-visuallyhidden" for="search-text">Search:</label>
-    <input class="b-siteSearch__input" type="search" name="s" id="search-text" value="" spellcheck="true" />
+    <label class="b-siteSearch__label g-visuallyhidden">Search:</label>
+    <input class="b-siteSearch__input" type="search" type="search" name="s" value="" spellcheck="true" />
     <input class="b-siteSearch__button" type="submit" value="Search" />
   </form>
 </div>
