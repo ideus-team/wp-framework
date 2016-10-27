9e81bbe9560c7b560fcabadc8780e3e74793dd58
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-10-27 17:07:18 +0300

    fix mu-plugins

--- wp-content/mu-plugins/nc-load.php
+++ wp-content/mu-plugins/nc-load.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: WP-framework
-Plugin URI: 
+Plugin URI:
 Description: Functions for WP-framework
 Author: iDeus
-Version: 
+Version:
 Author URI: http://ideus.biz
 */
 
@@ -22,9 +22,6 @@ require WPMU_PLUGIN_DIR.'/nc-functions/walkers.php';
 
 // Pagination
 require WPMU_PLUGIN_DIR.'/nc-functions/pagination.php';
-
-// Thumbnail
-require WPMU_PLUGIN_DIR.'/nc-functions/thumbnail.php';
 
 // Custom Post Types
 require WPMU_PLUGIN_DIR.'/nc-functions/post-type.php';
