cf44006a14be91f0567647437adefbec50036275
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-05-05 21:45:26 +0300

    Update jQuery

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -62,7 +62,7 @@ function nc_scripts() {
   wp_register_script( 'modernizr', get_theme_file_uri( 'assets/js/vendor/modernizr-2.8.3.min.js' ), false, '2.8.3' );
 
   wp_deregister_script( 'jquery' );
-  wp_register_script( 'jquery', get_theme_file_uri( 'assets/js/vendor/jquery-3.1.1.min.js' ), false, '3.1.1' );
+  wp_register_script( 'jquery', get_theme_file_uri( 'assets/js/vendor/jquery-3.2.1.min.js' ), false, '3.2.1' );
 
   wp_enqueue_script( 'js-main', get_theme_file_uri( 'assets/js/scripts.js' ), array( 'modernizr', 'jquery' ), filemtime( get_theme_file_path( 'assets/js/scripts.js' ) ), true );
   // wp_enqueue_script( 'js-extra', get_theme_file_uri( 'assets/js/scripts-extra.js' ), array( 'js-main' ), filemtime( get_theme_file_path( 'assets/js/scripts-extra.js' ) ), true );
