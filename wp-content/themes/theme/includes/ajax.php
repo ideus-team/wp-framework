ddcefd72d6a19064adc2e54971897c152aa5efe6
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-07-24 17:46:26 +0300

    modify ajax

--- wp-content/themes/theme/includes/ajax.php
+++ wp-content/themes/theme/includes/ajax.php
@@ -6,15 +6,19 @@
 add_action( 'wp_ajax_ncAction', 'ncAction_callback' );
 add_action( 'wp_ajax_nopriv_ncAction', 'ncAction_callback' );
 function ncAction_callback() {
-  $args = wp_parse_args( $_POST, array(
-    'test' => false,
-  ) );
+  $result = array();
 
-  $result = array(
-    'test' => $args['test'],
-  );
+  if ( ! $_POST['postdata'] ) {
+    $result['error'] = 'Empty postdata';
+  } else {
+    $args = wp_parse_args( $_POST['postdata'], array(
 
-  if ( $result['test'] ) {
+    ) );
+
+    $result = $args;
+  }
+
+  if ( ! $result['error'] ) {
     wp_send_json_success( $result );
   } else {
     wp_send_json_error( $result );
