e39d2fd4921dfeb28fe3fb923580521e14971fc1
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-09-08 05:58:44 +0300

    remove old post_meta functions

--- wp-content/mu-plugins/nc-load.php
+++ wp-content/mu-plugins/nc-load.php
@@ -29,8 +29,5 @@ require WPMU_PLUGIN_DIR . '/nc-functions/post-type.php';
 // Custom Taxonomies
 require WPMU_PLUGIN_DIR . '/nc-functions/taxonomy.php';
 
-// Post Meta
-require WPMU_PLUGIN_DIR . '/nc-functions/postmeta.php';
-
 // Breadcrumbs
 require WPMU_PLUGIN_DIR . '/nc-functions/breadcrumbs.php';
