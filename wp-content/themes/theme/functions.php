39ff18b971bd1aeaf2c8c791d843a19e0feeecd3
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-11-09 21:12:30 +0200

    Refactoring functions.php

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * Constants
- */
-// define( 'NAME', 'value' );
+require_once( get_theme_file_path( 'includes/constants.php' ) );
 
 /**
  * Content Width
@@ -123,6 +120,7 @@ function nc_disable_plugin_deactivation( $actions, $plugin_file ) {
   $important_plugins = array(
     'advanced-custom-fields-pro/acf.php',
     'contact-form-7/wp-contact-form-7.php',
+    'polylang/polylang.php',
   );
   if ( in_array( $plugin_file, $important_plugins ) ) {
     unset( $actions['deactivate'] );
@@ -131,81 +129,6 @@ function nc_disable_plugin_deactivation( $actions, $plugin_file ) {
   return $actions;
 }
 
-
-/**
- * Modify except
- */
-function nc_excerpt( $num_words = 25, $more = '… →' ) {
-  $excerpt = wp_trim_words( get_the_excerpt(), $num_words, $more );
-  echo apply_filters( 'the_excerpt', $excerpt );
-}
-
-add_filter( 'excerpt_more', 'nc_excerpt_more' );
-function nc_excerpt_more( $more ) {
-  return '…';
-}
-
-add_filter( 'excerpt_length', 'nc_excerpt_length' );
-function nc_excerpt_length( $length ) {
-  return 20;
-}
-
-function nc_tel( $phone = '' ) {
-  $patterns[0] = '/\ /';
-  $patterns[1] = '/\./';
-  $patterns[2] = '/\(/';
-  $patterns[3] = '/\)/';
-  $patterns[4] = '/\-/';
-
-  return preg_replace( $patterns, '', $phone );
-}
-
-
-/**
- * Breadcrumbs settings
- */
-add_filter( 'kama_breadcrumbs_default_args', 'nc_breadcrumbs_default_args' );
-function nc_breadcrumbs_default_args( $args ) {
-  $args_new = array(
-    'on_front_page'   => true,
-    'show_post_title' => true,
-    'show_term_title' => true,
-    'title_patt'      => '<li class="b-breadcrumbs__item -state_current">%s</li>',
-    'last_sep'        => true,
-    'markup'          => 'schema.org',
-    'priority_tax'    => array( 'category' ),
-    'priority_terms'  => array(),
-    'nofollow'        => false,
-  );
-
-  $args = wp_parse_args( $args_new, $args );
-
-  return $args;
-}
-
-/**
- * Breadcrumbs localization
- */
-add_filter( 'kama_breadcrumbs_default_loc', 'nc_breadcrumbs_default_loc' );
-function nc_breadcrumbs_default_loc( $l10n ) {
-  $l10n_new = array(
-    'home'       => 'Front page',
-    'paged'      => 'Page %d',
-    '_404'       => 'Error 404',
-    'search'     => 'Search results by query - <b>%s</b>',
-    'author'     => 'Author archive: <b>%s</b>',
-    'year'       => 'Archive by <b>%d</b> year',
-    'month'      => 'Archive by: <b>%s</b>',
-    'day'        => '',
-    'attachment' => 'Media: %s',
-    'tag'        => 'Posts by tag: <b>%s</b>',
-    'tax_tag'    => '%1$s from "%2$s" by tag: <b>%3$s</b>',
-  );
-
-  $l10n = wp_parse_args( $l10n_new, $l10n );
-
-  return $l10n;
-}
 
 /**
  * Hide the Advanced Custom Fields menu
@@ -218,5 +141,5 @@ function nc_breadcrumbs_default_loc( $l10n ) {
  */
 require_once( get_theme_file_path( 'includes/ajax.php' ) );
 require_once( get_theme_file_path( 'includes/options.php' ) );
-require_once( get_theme_file_path( 'includes/polylang.php' ) );
+// require_once( get_theme_file_path( 'includes/polylang.php' ) );
 require_once( get_theme_file_path( 'includes/other.php' ) );
