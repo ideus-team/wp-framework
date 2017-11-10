35733dca0a581cc1600ad59ce15a3e848469f53f
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-11-10 17:53:42 +0200

    fix polylang strins file

--- wp-content/themes/theme/includes/polylang.php
+++ wp-content/themes/theme/includes/polylang.php
@@ -3,3 +3,6 @@
  * Polylang Strings
  * Example: pll_register_string( $name, $string, $group, $multiline );
  */
+if ( function_exists( 'pll_register_string' ) ) {
+  // pll_register_string( $name, $string, $group, $multiline );
+}
