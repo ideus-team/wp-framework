0f3e4c6c166e46018bf5b09f0615bd4788068014
Valery Bogdanov <valery.bogdanov@ideus.biz>
2018-03-23 15:14:51 +0200

    fix styles init

--- wp-content/themes/theme/includes/theme-styles.php
+++ wp-content/themes/theme/includes/theme-styles.php
@@ -5,8 +5,7 @@
  */
 add_action( 'wp_enqueue_scripts', 'nc_styles' );
 function nc_styles() {
-  $protocol = is_ssl() ? 'https' : 'http';
-  // wp_enqueue_style( 'googlefonts', $protocol . '://fonts.googleapis.com/css?family=Lato:100,300,400,600,700,900|Open+Sans:300,400,600,700,800', false, null );
+  // wp_enqueue_style( 'googlefonts', '//fonts.googleapis.com/css?family=Lato:100,300,400,600,700,900|Open+Sans:300,400,600,700,800', false, null );
 
   wp_enqueue_style( 'css-main', get_theme_file_uri( 'assets/css/main.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/main.min.css' ) ) );
 }
