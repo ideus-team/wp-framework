8d63ac89149e5cd1e2e9ec4c3d16d94a581cefa9
Valery Bogdanov <valery.bogdanov@ideus.biz>
2018-09-13 19:24:47 +0300

    Example Shortcode

--- wp-content/themes/theme/includes/shortcodes.php
+++ wp-content/themes/theme/includes/shortcodes.php
@@ -2,3 +2,23 @@
 /**
  * Shortcodes
  */
+
+// Example Shortcode
+/*
+add_shortcode( 'nc_test', 'nc_test_shortcode' );
+function nc_test_shortcode( $atts, $content ) {
+  $atts = shortcode_atts( array(
+
+  ), $atts );
+
+  ob_start();
+  ?>
+
+  Test
+
+  <?php
+  $content = ob_get_clean();
+
+  return $content;
+}
+*/
