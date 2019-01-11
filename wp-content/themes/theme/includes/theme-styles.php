f388efc4c598ba0216c52cc3f437b221a402b2c9
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-01-11 18:46:58 +0200

    some fix in enqueue style

--- wp-content/themes/theme/includes/theme-styles.php
+++ wp-content/themes/theme/includes/theme-styles.php
@@ -7,7 +7,9 @@ add_action( 'wp_enqueue_scripts', 'nc_styles' );
 function nc_styles() {
   // wp_enqueue_style( 'googlefonts', '//fonts.googleapis.com/css?family=Lato:100,300,400,600,700,900|Open+Sans:300,400,600,700,800', false, null );
 
-  wp_enqueue_style( 'css-main', get_theme_file_uri( 'assets/css/main.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/main.min.css' ) ) );
+  if ( file_exists( get_theme_file_path( 'assets/css/main.min.css' ) ) ) {
+    wp_enqueue_style( 'css-main', get_theme_file_uri( 'assets/css/main.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/main.min.css' ) ) );
+  }
 }
 
 
@@ -16,14 +18,19 @@ function nc_styles() {
  */
 add_action( 'admin_enqueue_scripts', 'nc_admin_styles' );
 function nc_admin_styles() {
-  wp_enqueue_style( 'css-admin', get_theme_file_uri( 'assets/css/admin.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/admin.min.css' ) ) );
+  if ( file_exists( get_theme_file_path( 'assets/css/admin.min.css' ) ) ) {
+    wp_enqueue_style( 'css-admin', get_theme_file_uri( 'assets/css/admin.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/admin.min.css' ) ) );
+  }
 }
 
 
 /**
  * Login styles
  */
-// add_action( 'login_enqueue_scripts', 'nc_login_styles' );
+add_action( 'login_enqueue_scripts', 'nc_login_styles' );
 function nc_login_styles() {
-  wp_enqueue_style( 'css-login', get_theme_file_uri( 'assets/css/login.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/login.min.css' ) ) );
+  if ( file_exists( get_theme_file_path( 'assets/css/login.min.css' ) ) ) {
+    wp_enqueue_style( 'css-login', get_theme_file_uri( 'assets/css/login.min.css' ), false, filemtime( get_theme_file_path( 'assets/css/login.min.css' ) ) );
+  }
 }
+
