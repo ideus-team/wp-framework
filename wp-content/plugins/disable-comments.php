fdac17e1398784a3d7b0333face1d2d58d1ff60e
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-02-07 20:06:48 +0200

    add empty plugins file

---
+++ wp-content/plugins/disable-comments.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Disable Comments
+Plugin URI: https://wordpress.org/plugins/disable-comments/
+Description: Allows administrators to globally disable comments on their site. Comments can be disabled according to post type.
+Version: 0.0.1
+Author: Samir Shah
+Author URI: http://www.rayofsolaris.net/
+License: GPL2
+Text Domain: disable-comments
+Domain Path: /languages/
+*/
