0dfa7d5ed9d603507443b12fecb15429fe3dbe30
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-02-07 19:31:45 +0200

    add empty plugins file

---
+++ wp-content/plugins/auto-domain-change.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Automatic Domain Changer
+Plugin URI: http://www.nuagelab.com/wordpress-plugins/auto-domain-change
+Description: Automatically changes the domain of a WordPress blog
+Author: NuageLab <wordpress-plugins@nuagelab.com>
+Version: 0.0.1
+License: GPLv2 or later
+Author URI: http://www.nuagelab.com/wordpress-plugins
+*/
---
+++ wp-content/plugins/classic-editor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Classic Editor
+ *
+ * Plugin Name: Classic Editor
+ * Plugin URI:  https://wordpress.org/plugins/classic-editor/
+ * Description: Enables the WordPress classic editor and the old-style Edit Post screen with TinyMCE, Meta Boxes, etc. Supports the older plugins that extend this screen.
+ * Version:     0.0.1
+ * Author:      WordPress Contributors
+ * Author URI:  https://github.com/WordPress/classic-editor/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: classic-editor
+ * Domain Path: /languages
+ */
