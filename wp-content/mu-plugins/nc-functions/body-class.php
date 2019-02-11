d852382b2f2b3e36e5be9ad47126c619b9dc165a
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-02-11 19:18:01 +0200

    fix nc_body_class

--- wp-content/mu-plugins/nc-functions/body-class.php
+++ wp-content/mu-plugins/nc-functions/body-class.php
@@ -6,20 +6,19 @@ add_filter( 'body_class', 'nc_body_class' );
 function nc_body_class( $classes ) {
   global $post;
 
-  if ( ! $post ) {
-    return array();
-  }
-
   if ( is_front_page() ) {
     // Homepage
     $classes[] = '-page_home';
   } else {
     $classes[] = '-page_inner';
 
-    if ( is_home() ) {
+    if ( is_404() ) {
+      // Page 404
+      $slug = '404';
+    } elseif ( is_home() ) {
       // Posts page
       $slug = get_queried_object()->post_name;
-    } else {
+    } elseif ( $post ) {
       // Inner Page
       $post_data = get_post( $post->ID, ARRAY_A );
       $slug = $post_data['post_name'];
