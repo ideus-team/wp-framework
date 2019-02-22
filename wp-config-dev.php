e4e45909304706cb3dd6558fb92af7eca69ce343
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-02-22 13:54:54 +0200

    update configs

--- wp-config-dev.php
+++ wp-config-dev.php
@@ -1,15 +1,15 @@
 <?php
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define( 'DB_NAME', 'database_name_here' );
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define( 'DB_USER', 'username_here' );
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define( 'DB_PASSWORD', 'password_here' );
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define( 'DB_HOST', 'localhost' );
 
 define( 'WP_DEBUG', true );
 define( 'WP_LOCAL_DEV', true );
