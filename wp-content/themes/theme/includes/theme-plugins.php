729e1bab0ab946fc77c49b0c614ed10bcdb4255f
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-04-05 16:45:46 +0300

    Contact Form 7 default select value

--- wp-content/themes/theme/includes/theme-plugins.php
+++ wp-content/themes/theme/includes/theme-plugins.php
@@ -79,3 +79,24 @@ function nc_breadcrumbs_blog_link( $false, $linkpatt, $sep, $ptype ) {
   $page = get_post( $post_id );
   return sprintf( $linkpatt, get_permalink( $page ), $page->post_title ) . $sep;
 }
+
+
+/**
+ * Contact Form 7 default select value
+ */
+// add_filter( 'wpcf7_form_elements', 'nc_wpcf7_form_elements' );
+function nc_wpcf7_form_elements( $html ) {
+  nc_replace_include_blank( 'interested', 'I am interested in…', $html );
+
+  return $html;
+}
+
+function nc_replace_include_blank( $name, $text, &$html ) {
+  $matches = false;
+  preg_match( '/<select name="' . $name . '"[^>]*>(.*)<\/select>/iU', $html, $matches );
+
+  if ( $matches ) {
+    $select = str_replace( '<option value="">---</option>', '<option value="">' . $text . '</option>', $matches[0] );
+    $html = preg_replace( '/<select name="' . $name . '"[^>]*>(.*)<\/select>/iU', $select, $html );
+  }
+}
