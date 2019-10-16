0f0c1f8bc8b77490b4e13922fc3a379f1d324b89
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2019-10-16 13:39:59 +0300

    add get_the_slug & the_slug

--- wp-content/themes/theme/includes/functions.php
+++ wp-content/themes/theme/includes/functions.php
@@ -46,3 +46,31 @@ function nc_get_youtube_id( $url ) {
 
   return $video_id;
 }
+
+
+/**
+ * Get the page or post slug
+ */
+if ( ! function_exists( 'get_the_slug' ) ) {
+  function get_the_slug( $post = 0 ) {
+    $post = get_post( $post );
+
+    $slug = isset( $post->post_name ) ? $post->post_name : '';
+    $id = isset( $post->ID ) ? $post->ID : 0;
+
+    return apply_filters( 'the_slug', $slug, $id );
+  }
+}
+
+/**
+ * Display the page or post slug
+ *
+ * Uses get_the_slug()
+ */
+if ( ! function_exists( 'the_slug' ) ) {
+  function the_slug() {
+    $slug = get_the_slug();
+
+    echo $slug;
+  }
+}
