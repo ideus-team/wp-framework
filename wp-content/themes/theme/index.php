2cd4e3f494ec0c141e740510696104afbbe13c61
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2019-11-06 12:21:41 +0200

    codestyle fix

--- wp-content/themes/theme/index.php
+++ wp-content/themes/theme/index.php
@@ -3,10 +3,12 @@
 <main class="l-contentText">
 
   <?php while ( have_posts() ) : the_post(); ?>
+
     <section class="b-contentText">
       <h2 class="b-contentText__title"><?php the_title(); ?></h2>
       <div class="b-contentText__content b-text"><?php the_content(); ?></div>
     </section>
+
   <?php endwhile; ?>
 
   <?php nc_pagenavi(); ?>
