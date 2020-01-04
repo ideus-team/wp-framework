3c14d4d013455f41a7832f09eefb2c97e89ea732
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-01-04 16:45:12 +0200

    remove feed links

--- wp-content/themes/theme/includes/theme-setup.php
+++ wp-content/themes/theme/includes/theme-setup.php
@@ -19,7 +19,8 @@ function nc_setup() {
   load_theme_textdomain( 'nc_theme', get_template_directory() . '/languages' );
 
   remove_action( 'wp_head', 'wp_generator' );
-  // remove_action( 'wp_head', 'feed_links_extra', 3 );
+  remove_action( 'wp_head', 'feed_links_extra', 3 );
+  remove_action( 'wp_head', 'feed_links', 2 );
   remove_action( 'wp_head', 'rsd_link' );
   remove_action( 'wp_head', 'wlwmanifest_link' );
   // remove_action( 'wp_head', 'index_rel_link' );
