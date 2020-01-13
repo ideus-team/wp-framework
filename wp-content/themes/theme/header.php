cb5f4effebe1d0410f4ac9b59dfcee0122a71ccc
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-01-13 20:41:18 +0200

    Some fix codestyle

--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html class="l-html no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8" />
+  <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <style>
     @-ms-viewport {
       width: device-width;
@@ -16,13 +16,13 @@
 
   <?php if ( ! has_site_icon() ) : ?>
 
-    <link rel="shortcut icon" href="<?php echo home_url( '/favicon.ico' ); ?>" />
-    <link rel="manifest" href="<?php echo home_url( '/site.webmanifest' ); ?>" />
-    <link rel="apple-touch-icon" href="<?php echo home_url( '/icon.png' ); ?>" />
+    <link rel="shortcut icon" href="<?php echo home_url( '/favicon.ico' ); ?>">
+    <link rel="manifest" href="<?php echo home_url( '/site.webmanifest' ); ?>">
+    <link rel="apple-touch-icon" href="<?php echo home_url( '/icon.png' ); ?>">
 
   <?php endif; ?>
 
-  <!--<meta name="theme-color" content="#ed1c24" />-->
+  <!--<meta name="theme-color" content="#ed1c24">-->
 
   <?php wp_head(); ?>
   <?php get_template_part( 'template-parts/scripts/header' ); ?>
@@ -44,7 +44,7 @@
           ?>
           <<?php echo $siteLogo__tag; ?> class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
             <a class="b-siteLogo__link"<?php echo $siteLogo__link; ?> itemprop="url">
-              <img class="b-siteLogo__icon" src="<?php echo get_theme_file_uri( 'assets/img/blocks/siteLogo/siteLogo-logo.png' ); ?>" alt="<?php bloginfo( 'name' ); ?>" title="<?php bloginfo( 'name' ); ?>" itemprop="logo" />
+              <img class="b-siteLogo__icon" src="<?php echo get_theme_file_uri( 'assets/img/blocks/siteLogo/siteLogo-logo.png' ); ?>" alt="<?php bloginfo( 'name' ); ?>" title="<?php bloginfo( 'name' ); ?>" itemprop="logo">
             </a>
           </<?php echo $siteLogo__tag; ?>>
         </div>
--- wp-content/themes/theme/searchform.php
+++ wp-content/themes/theme/searchform.php
@@ -2,9 +2,9 @@
   <form class="b-siteSearch" role="search" action="<?php echo home_url(); ?>" method="get">
     <label class="b-siteSearch__search">
       <span class="b-siteSearch__label g-visuallyhidden"><?php _e( 'Search' ); ?>:</span>
-      <input class="b-siteSearch__input" type="search" name="s" spellcheck="true" />
+      <input class="b-siteSearch__input" type="search" name="s" spellcheck="true">
     </label>
 
-    <input class="b-siteSearch__button" type="submit" value="<?php _e( 'Search' ); ?>" />
+    <input class="b-siteSearch__button" type="submit" value="<?php _e( 'Search' ); ?>">
   </form>
 </div>
