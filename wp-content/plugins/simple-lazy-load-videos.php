389a07286d88ce335e6df84ebaccebec69ddb15c
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-02-05 16:50:33 +0200

    Add Simple Lazy Load Videos

---
+++ wp-content/plugins/simple-lazy-load-videos.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Simple Lazy Load Videos
+Plugin URI:  https://github.com/radkill/simple-lazy-load-videos
+Description: Simple Lazy Load for embedded video from Youtube and Vimeo
+Author:      Valerii Bohdanov
+Author URI:  https://profiles.wordpress.org/rad_/
+Text Domain: simple-lazy-load-videos
+Domain Path: /languages/
+Version: 0.0.1
+*/
