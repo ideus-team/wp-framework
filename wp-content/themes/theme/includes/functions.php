71626c854d6de6fde548db19fbeaaab62f44b2ab
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-12-16 19:12:39 +0200

    Add nc_remote_api_get

--- wp-content/themes/theme/includes/functions.php
+++ wp-content/themes/theme/includes/functions.php
@@ -49,6 +49,33 @@ function nc_get_youtube_id( $url ) {
 
 
 /**
+ * Get remote JSON & cache with Transients API
+ */
+function nc_remote_api_get( $api_url, $expiration = HOUR_IN_SECONDS ) {
+  $api_url_hash = 'nc_cache_' . md5( $api_url );
+  $cache = get_transient( $api_url_hash );
+
+  if ( $cache ) {
+    $body = $cache;
+  } else {
+    $request = wp_remote_get( $api_url );
+
+    if ( is_wp_error( $request ) ) {
+      return false;
+    }
+
+    $body = wp_remote_retrieve_body( $request );
+
+    if ( $expiration ) {
+      set_transient( $api_url_hash, $body, $expiration );
+    }
+  }
+
+  return json_decode( $body );
+}
+
+
+/**
  * Get the page or post slug
  */
 if ( ! function_exists( 'get_the_slug' ) ) {
