0cd566ba2afb831063a3f9d521738281abf84e37
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2021-11-09 14:55:54 +0200

    Removed modernizr

--- wp-content/themes/theme/includes/theme-scripts.php
+++ wp-content/themes/theme/includes/theme-scripts.php
@@ -4,13 +4,11 @@
  */
 add_action( 'wp_enqueue_scripts', 'nc_scripts' );
 function nc_scripts() {
-  wp_register_script( 'modernizr', get_theme_file_uri( 'assets/js/vendor/modernizr-3.11.2.min.js' ), false, '3.11.2', true );
-
   wp_deregister_script( 'jquery' );
   wp_register_script( 'jquery', get_theme_file_uri( 'assets/js/vendor/jquery-3.6.0.min.js' ), false, '3.6.0', true );
 
   if ( file_exists( get_theme_file_path( 'assets/js/scripts.js' ) ) ) {
-    wp_enqueue_script( 'nc-main', get_theme_file_uri( 'assets/js/scripts.js' ), array( 'modernizr', 'jquery' ), filemtime( get_theme_file_path( 'assets/js/scripts.js' ) ), true );
+    wp_enqueue_script( 'nc-main', get_theme_file_uri( 'assets/js/scripts.js' ), array( 'jquery' ), filemtime( get_theme_file_path( 'assets/js/scripts.js' ) ), true );
   }
 
   /**
