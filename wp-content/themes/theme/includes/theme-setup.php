458b168475919dc6e63a0b3746700e6f72743beb
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2022-10-23 03:35:34 +0300

    Add all post-formats

--- wp-content/themes/theme/includes/theme-setup.php
+++ wp-content/themes/theme/includes/theme-setup.php
@@ -53,7 +53,7 @@ function nc_setup() {
   /**
    * This feature enables Post Formats support for a Theme.
    */
-  // add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
+  // add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 
   /**
    * Styles for editor
