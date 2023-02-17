726c7ad4e8ab2cb376569feff5bc41c3b66bb1c0
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-02-17 16:29:21 +0200

    Fix default plugins names

--- wp-content/plugins/wp-mail-smtp.php
+++ wp-content/plugins/wp-mail-smtp.php
@@ -2,7 +2,7 @@
 /**
  * WP Mail SMTP
  *
- * Plugin Name: Plugin Name: WP Mail SMTP
+ * Plugin Name: WP Mail SMTP
  * Plugin URI:  https://wpmailsmtp.com/
  * Description: Reconfigures the <code>wp_mail()</code> function to use Gmail/Mailgun/SendGrid/SMTP instead of the default <code>mail()</code> and creates an options page to manage the settings.
  * Version:     0.0.1
