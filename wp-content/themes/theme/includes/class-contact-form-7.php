fd1db34f748531f44457e44e917d25dbc0520a31
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-08-09 19:35:59 +0300

    Remove "Default empty option for CF7 select" method, we can use 'first_as_label' attribute for it.

--- wp-content/themes/theme/includes/class-contact-form-7.php
+++ wp-content/themes/theme/includes/class-contact-form-7.php
@@ -27,47 +27,6 @@ if ( ! class_exists( 'iDeus\Theme\Contact_Form_7' ) ) {
 
       // Do not apply the 'autop' filter to the form content
       add_filter( 'wpcf7_autop_or_not', '__return_false' );
-
-      // Set default empty option for select
-      // add_filter( 'wpcf7_form_elements', array( $this, 'select_empty_option' ) );
-    }
-
-
-    /**
-     * Set default empty option for select.
-     *
-     * @since 2.0.2
-     *
-     * @param  string $html Form HTML
-     * @return string
-     */
-    public function select_empty_option( $html ) {
-      $this->replace_include_blank( 'location', 'Select One', $html );
-
-      return $html;
-    }
-
-
-    /**
-     * Replace empty option with custom.
-     *
-     * @since 2.0.2
-     *
-     * @param  string $name  Select name
-     * @param  string $text  Select text
-     * @param  string $html  Form HTML
-     * @return string        Form HTML
-     */
-    private function replace_include_blank( $name, $text, $html ) {
-      $matches = false;
-      preg_match( '/<select name="' . $name . '"[^>]*>(.*)<\/select>/iU', $html, $matches );
-
-      if ( $matches ) {
-        $select = str_replace( '<option value="">---</option>', '<option value="">' . $text . '</option>', $matches[0] );
-        $html = preg_replace( '/<select name="' . $name . '"[^>]*>(.*)<\/select>/iU', $select, $html );
-      }
-
-      return $html;
     }
 
   }
