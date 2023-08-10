24b72c2b49cec247d260683a4077b9a20e851ef3
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-08-11 01:36:10 +0300

    Refactor \iDeus\Theme\Resources

--- wp-content/themes/theme/includes/class-resources.php
+++ wp-content/themes/theme/includes/class-resources.php
@@ -45,6 +45,44 @@ if ( ! class_exists( 'iDeus\Theme\Resources' ) ) {
 
 
     /**
+     * Scripts
+     *
+     * @since 2.0.0
+     */
+    public function scripts() {
+      wp_deregister_script( 'jquery' );
+      wp_register_script(
+        'jquery',
+        get_theme_file_uri( 'assets/js/vendor/jquery-3.7.0.min.js' ),
+        false,
+        '3.7.0',
+        true
+      );
+
+      if ( file_exists( get_theme_file_path( 'assets/js/scripts.js' ) ) ) {
+        wp_enqueue_script(
+          'nc-main',
+          get_theme_file_uri( 'assets/js/scripts.js' ),
+          array( 'jquery' ),
+          filemtime( get_theme_file_path( 'assets/js/scripts.js' ) ),
+          true
+        );
+      }
+
+      /**
+       * Variables for JS (nc_params.ajax_url, nc_params.home_url, nc_params.theme_url, etc.)
+       *
+       * @since 2.0.0
+       */
+      wp_localize_script( 'nc-main', 'nc_params', array(
+        'ajax_url'  => admin_url( 'admin-ajax.php' ),
+        'home_url'  => home_url(),
+        'theme_url' => get_stylesheet_directory_uri(),
+      ) );
+    }
+
+
+    /**
      * Styles
      *
      * Use 'get_footer' instead 'wp_enqueue_scripts' hook for move CSS to footer.
@@ -101,44 +139,6 @@ if ( ! class_exists( 'iDeus\Theme\Resources' ) ) {
           filemtime( get_theme_file_path( 'assets/css/login.min.css' ) )
         );
       }
-    }
-
-
-    /**
-     * Scripts
-     *
-     * @since 2.0.0
-     */
-    public function scripts() {
-      wp_deregister_script( 'jquery' );
-      wp_register_script(
-        'jquery',
-        get_theme_file_uri( 'assets/js/vendor/jquery-3.7.0.min.js' ),
-        false,
-        '3.7.0',
-        true
-      );
-
-      if ( file_exists( get_theme_file_path( 'assets/js/scripts.js' ) ) ) {
-        wp_enqueue_script(
-          'nc-main',
-          get_theme_file_uri( 'assets/js/scripts.js' ),
-          array( 'jquery' ),
-          filemtime( get_theme_file_path( 'assets/js/scripts.js' ) ),
-          true
-        );
-      }
-
-      /**
-       * Variables for JS (nc_params.ajax_url, nc_params.home_url, nc_params.theme_url, etc.)
-       *
-       * @since 2.0.0
-       */
-      wp_localize_script( 'nc-main', 'nc_params', array(
-        'ajax_url'  => admin_url( 'admin-ajax.php' ),
-        'home_url'  => home_url(),
-        'theme_url' => get_stylesheet_directory_uri(),
-      ) );
     }
 
 
