73250d24fa7d5335394d49a15cc12751c7df1093
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-09-12 03:21:54 +0300

    Go to 2.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 2.2.4
+* Fix errors if ACF plugin is disabled
+
 ### 2.2.3
 * Update changelog
 
--- wp-content/mu-plugins/wp-framework.php
+++ wp-content/mu-plugins/wp-framework.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ideus-team/wp-framework
  * Description: Functions for WP-framework
  * Author: iDeus
- * Version: 2.2.3
+ * Version: 2.2.4
  * Author URI: https://ideus.biz
  */
 
