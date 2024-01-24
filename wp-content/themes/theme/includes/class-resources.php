aed9cac3431ca26d715b32efe5fc35dfef50ff25
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2024-01-24 14:41:47 +0200

    Disable Gutenberg CSS

--- wp-content/themes/theme/includes/class-resources.php
+++ wp-content/themes/theme/includes/class-resources.php
@@ -177,7 +177,7 @@ if ( ! class_exists( 'iDeus\Theme\Resources' ) ) {
 
 
 		/**
-		 * Remove global styles.
+		 * Disable global styles.
 		 *
 		 * @since 2.0.0
 		 */
@@ -188,12 +188,15 @@ if ( ! class_exists( 'iDeus\Theme\Resources' ) ) {
 
 
 		/**
-		 * Remove Gutenberg CSS.
+		 * Disable Gutenberg CSS.
 		 *
 		 * @since 2.0.0
 		 */
 		public function deregister_styles() {
 			wp_dequeue_style( 'wp-block-library' );
+			wp_dequeue_style( 'wp-block-library-theme' );
+			wp_dequeue_style( 'wc-block-style' );
+			wp_dequeue_style( 'classic-theme-styles' );
 		}
 	}
 }
