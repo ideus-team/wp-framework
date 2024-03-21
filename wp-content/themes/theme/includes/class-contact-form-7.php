728e574779c13f0e1335a4a5e3929a5c260087bc
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2024-03-21 18:46:00 +0200

    Disable 'Unsafe email config is used without sufficient protection' check.

--- wp-content/themes/theme/includes/class-contact-form-7.php
+++ wp-content/themes/theme/includes/class-contact-form-7.php
@@ -26,6 +26,27 @@ if ( ! class_exists( 'iDeus\Theme\Contact_Form_7' ) ) {
 
 			// Do not apply the 'autop' filter to the form content.
 			add_filter( 'wpcf7_autop_or_not', '__return_false' );
+
+			// Disable 'Unsafe email config is used without sufficient protection' check.
+			add_filter( 'wpcf7_config_validator_available_error_codes', array( $this, 'disable_errors_check' ), 10, 2 );
+		}
+
+
+		/**
+		 * Disable 'Unsafe email config is used without sufficient protection' check.
+		 *
+		 * @since X.X.X
+		 * @link https://contactform7.com/2023/10/15/disabling-only-specific-error-types-of-config-validator/
+		 */
+		public function disable_errors_check( $error_codes, $contact_form ) {
+			// List error codes to disable here.
+			$error_codes_to_disable = array(
+				'unsafe_email_without_protection',
+			);
+
+			$error_codes = array_diff( $error_codes, $error_codes_to_disable );
+
+			return $error_codes;
 		}
 	}
 }
