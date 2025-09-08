c0ff1a99fbbb241f7de9dabeee8242216d2388d5
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2025-09-08 14:48:37 +0300

    Go to 2.21.6

--- wp-content/mu-plugins/wp-framework.php
+++ wp-content/mu-plugins/wp-framework.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://github.com/ideus-team/wp-framework
  * Description: Functions for WP-framework
  * Author:      iDeus
- * Version:     2.21.5
+ * Version:     2.21.6
  * Author URI:  https://ideus.biz
  *
  * @package WP-framework
